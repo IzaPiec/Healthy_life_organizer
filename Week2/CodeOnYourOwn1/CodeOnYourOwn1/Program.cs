b70fe268fa6e840ecdade77eba7798dc0dc0045d
IzaPiec <izabelapiec@gmail.com>
2016-02-25 00:02:38 +0100

    Add CodeOnYourOwn#1 task from week2

---
+++ Week2/CodeOnYourOwn1/CodeOnYourOwn1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeOnYourOwn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName=null;
+            Console.WriteLine("Please provide your name:");
+            myName= Console.ReadLine();
+            Console.WriteLine("Hello "+ myName+"!");
+            Console.ReadLine();
+
+        }
+    }
+}
